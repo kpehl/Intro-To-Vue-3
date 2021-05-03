b102d8e3e07f2b529845b328eabe4ea9943cd3e5
Kathleen Pehl <kathleen.pehl@gmail.com>
2021-05-03 13:51:55 -0400

    Lesson 5 - List Rendering

--- main.js
+++ main.js
@@ -6,7 +6,19 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             url: 'https://smile.amazon.com/s?k=socks',
             inventory: 8,
-            onSale: false
+            onSale: false,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' },
+            ],
+            sizes: [
+                { id: 1, size: 'Kids Small' },
+                { id: 2, size: 'Kids Large' },
+                { id: 3, size: 'Adult Small' },
+                { id: 4, size: 'Adult Medium' },
+                { id: 5, size: 'Adult Large' }
+            ]
         }
     }
 })
