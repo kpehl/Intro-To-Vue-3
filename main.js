66eda98a7eb9ef38fbf0fa357bd302693492daaa
Kathleen Pehl <kathleen.pehl@gmail.com>
2021-05-03 13:16:18 -0400

    Lesson 3 - Attribute Binding

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
         data() {
         return {
             product: 'Socks',
-            description: 'Warm fuzzy socks'
+            description: 'Warm fuzzy socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://smile.amazon.com/s?k=socks'
         }
     }
 })
